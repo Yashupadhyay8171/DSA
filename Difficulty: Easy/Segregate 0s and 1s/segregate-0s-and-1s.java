1c2e6107ccf69d9117bbd5f65a2d661a36c5a372
Yash Upadhyay <130153376+Yashupadhyay8171@users.noreply.github.com>
2026-08-22 22:23:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.java
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.java
@@ -1,24 +1,13 @@
-// User function Template for Java
-
 class Solution {
-    void segregate0and1(int[] nums) {
+    void segregate0and1(int[] arr) {
         // code here
         int i=0;
-        int j=nums.length-1;
-        int index=0;
-        while(index<=j){
-            if(nums[index]==0){
-                int temp=nums[i];
-                nums[i]=nums[index];
-                nums[index]=temp;
+        for(int index=0;index<arr.length;index++){
+            if(arr[index]!=1){
+                int temp=arr[index];
+                arr[index]=arr[i];
+                arr[i]=temp;
                 i++;
-                index++;
-            }
-            else{
-                int temp=nums[index];
-                nums[index]=nums[j];
-                nums[j]=temp;
-                j--;
             }
         }
     }
